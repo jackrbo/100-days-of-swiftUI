797c9744e7002aa21e34cadc89a6121b7c10dbfc
Jack Richard-Bollans <jack.richard-bollans@f-secure.com>
2021-06-29 16:59:06 +0300

    Initial template of project

---
+++ iExpense/iExpense/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iExpense
+//
+//  Created by Richard-Bollans, Jack on 29.6.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
