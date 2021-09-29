661d4f52aaf090f01bbff7175f6303dc41a0fc39
Jack Richard-Bollans <jack.richard-bollans@f-secure.com>
2021-09-29 19:01:25 +0300

    Day 51 complete

--- day50/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
+++ day50/CupcakeCorner/CupcakeCorner/Views/CheckoutView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -22,14 +26,46 @@ struct CheckoutView: View {
                     Text("Your total is \(self.order.cost, specifier: "%.2f")")
                         .font(.title)
                     
-                    Button("Place order") {
-                        
+                    Button("Place Order") {
+                        placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        let encoder = JSONEncoder()
+        guard let encoded = try? encoder.encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            guard let data = data else {
+                print("No data response:\(error?.localizedDescription ?? "Unknown error"). ")
+                return
+            }
+            let decoder = JSONDecoder()
+            guard let decodedOrder = try? decoder.decode(Order.self, from: data) else {
+                print("Couldn't decode data")
+                return
+            }
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way! To  \(decodedOrder.name) at address \(decodedOrder.streetAddress), \(decodedOrder.city), \(decodedOrder.zip)"
+            showingConfirmation = true
+        }.resume()
     }
 }
 
