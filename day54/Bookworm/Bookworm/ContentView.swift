158f94266a6aaf25455d75df5f4f7e10bc5717b2
Jack Richard-Bollans <jack.richard-bollans@f-secure.com>
2022-01-17 20:00:27 +0200

    day54

--- day54/Bookworm/Bookworm/ContentView.swift
+++ day54/Bookworm/Bookworm/ContentView.swift
@@ -9,47 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     @Environment(\.managedObjectContext) var moc
-    var body: some View {
-        VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-            }
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
 
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                try? moc.save()
+    @State private var showingAddScreen = false
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        Text(book.title ?? "Unknown Title")
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title")
+                                    .font(.headline)
+                                Text(book.author ?? "Unknown Author")
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
             }
-        }
+               .navigationTitle("Bookworm")
+               .toolbar {
+                   ToolbarItem(placement: .navigationBarTrailing) {
+                       Button {
+                           showingAddScreen.toggle()
+                       } label: {
+                           Label("Add Book", systemImage: "plus")
+                       }
+                   }
+               }
+               .sheet(isPresented: $showingAddScreen) {
+                   AddBookView()
+               }
+       }
     }
 }
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColours = [Color.white, Color.red]
-    var offColours = [Color.purple, Color(white: 0.6)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: isOn ? onColours : offColours, startPoint: .bottom, endPoint: .top))
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ day54/Bookworm/RatingView.swift
@@ -0,0 +1,20 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Richard-Bollans, Jack on 17/01/2022.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView()
+    }
+}
