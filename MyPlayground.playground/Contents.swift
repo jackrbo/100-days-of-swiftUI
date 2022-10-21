fe091a5bb4a7cc5c27f347bc2c3ee6b24dc2b98d
Richard-Bollans, Jack <Jack.Richard-Bollans@F-Secure.com>
2022-10-21 15:28:24 +0300

    day50 allowed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,32 @@
 import UIKit
 import Foundation
-let timeRemainingInSeconds = 3
-let date = Date().timeIntervalSince1970
-let timeToLock = date + Double(timeRemainingInSeconds)
-extension Bool {
-    var intValue: Int {
-        self ? 1 : 0
-    }
+import NaturalLanguage
+
+let text = "kissassa"
+let range: Range<String.Index> = text.startIndex..<text.endIndex
+let tagger = NLTagger(tagSchemes: [.lemma])
+//tagger.setLanguage(.italian, range: range)
+tagger.string = text
+                   
+
+//let orthography = NSOrthography.defaultOrthography(forLanguage: "fi-FI")
+//tagger.setOrthography(orthography, range: range)
+
+tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .word, scheme: .lemma) { tag, range in
+   let stemForm = tag?.rawValue ?? String(text[range])
+   print(stemForm, terminator: "")
+   return true
 }
 
-if true.intValue == 1 {
-    print("E")
-}
+print(tagger.dominantLanguage?.rawValue)
+
+let tokenizer = NLTokenizer(unit: .sentence)
+tokenizer.setLanguage(.finnish)
+tokenizer.string = text
+
+let tokens = tokenizer.tokens(for: text.startIndex..<text.endIndex)
+
+
+let finnish = NLLanguageRecognizer.dominantLanguage(for: text)
+
+
